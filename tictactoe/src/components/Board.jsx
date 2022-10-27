b885a74ca25317102063c4263d5e26fb3225be90
srtatsumi <soumyadeep1399@gmail.com>
2022-10-27 17:38:41 +0530

    added board state

--- tictactoe/src/components/Board.jsx
+++ tictactoe/src/components/Board.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  console.log(board);
+  const handleSquareClick = position => {
+    if (board[position]) {
+      return;
+    }
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+    setIsXNext(prev => !prev);
+  };
+
+  const renderSqaure = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleSquareClick(position);
+        }}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSqaure(0)}
+        {renderSqaure(1)}
+        {renderSqaure(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSqaure(3)}
+        {renderSqaure(4)}
+        {renderSqaure(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSqaure(6)}
+        {renderSqaure(7)}
+        {renderSqaure(8)}
       </div>
     </div>
   );
