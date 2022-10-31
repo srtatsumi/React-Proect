4124659d3985aa00c25dc5040874f7c49f60d9cd
srtatsumi <soumyadeep1399@gmail.com>
2022-10-31 16:09:02 +0530

    Edit React Router

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is a homepage
+      </Route>
+      <Route exact path="/login">
+        This is a login page
+      </Route>
+      <Route>This is 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
